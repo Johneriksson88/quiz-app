385bd17ac1bf6c5d0ca00f8c4da34c96773aba3e
Johneriksson88 <john.e.eriksson@gmail.com>
2022-07-25 21:13:40 +0000

    Fix error caused by loading script.js two times

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
-var startButton
-var nextButton
-var questionsContainer
-var welcomeText
-var randomizedQuestions
-var currentQuestionIndex
-var questionElement = document.getElementById('question')
-var answerButtonsElement = document.getElementById('answer-buttons')
+const startButton = document.getElementById('start-btn')
+var nextButton = document.getElementById('next-btn')
+var correct
+const questionContainerElement = document.getElementById('question-container')
+let shuffledQuestions, currentQuestionIndex
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+startButton.addEventListener('click', startGame)
+let score
+let scoreElement = document.getElementById('score')
 
-document.addEventListener("DOMContentLoaded", function() {
-    startButton = document.getElementById('start-btn')
-    startButton.addEventListener('click', startGame)
-})
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+} )
 
 function startGame() {
-    console.log("Started")
+    score = 0
+    console.log('Started game')
     startButton.classList.add('hide')
     questionsContainer = document.getElementById('quiz-container')
     questionsContainer.classList.remove('hide')
@@ -21,48 +24,82 @@ function startGame() {
     welcomeText.classList.add('hide')
     nextButton = document.getElementById('next-btn')
     nextButton.classList.remove('hide')
-    randomizedQuestions = questions.sort(() => Math.random() - 0.5)
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-    displayQuestion()
+    setNextQuestion()
 }
 
 function setNextQuestion() {
-    displayQuestion(randomizedQuestions[currentQuestionIndex])
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function displayQuestion(question) {
+function showQuestion(question) {
     questionElement.innerText = question.question
-    question.answer.forEach(answer => {
-        const button = document.createElement('button')  
-        button.innerText = answer.text
-        button.classList.add('btn')      
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-        console.log('Displaying question')
-    });
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
+  }
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide')
+    } else {
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+    }
+  }
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+    incrementScore
+  } else {
+    element.classList.add('incorrect')
+  }
 }
 
-function checkAnswer() {
-
+function clearStatusClass(element) {
+    if (correct) {
+        element.classList.remove('correct')
+    } else {
+        element.classList.remove('incorrect')
+    }
 }
 
-function calculateCorrectAnswer() {
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
 
+    }
 }
 
 function incrementScore() {
-
+    score++
+    scoreElement.document.innerText = score
 }
-
-
 
 var questions = [
     {
         question: 'What is the capital of Australia?',
-        answer: [
+        answers: [
             { text: 'Sydney', correct: false },
             { text: 'Canberra', correct: true },
             { text: 'Melbourne', correct: false},
@@ -71,7 +108,7 @@ var questions = [
     },
     {
         question: 'Which single person has won the most Academy Awards of all time?',
-        answer: [
+        answers: [
             { text: 'Christopher Lee', correct: false },
             { text: 'Walt Disney', correct: true },
             { text: 'Meryl Streep', correct: false},
@@ -80,7 +117,7 @@ var questions = [
     },
     {
         question: 'What year did World War I start?',
-        answer: [
+        answers: [
             { text: '1914', correct: true },
             { text: '1899', correct: false },
             { text: '1938', correct: false},
@@ -89,7 +126,7 @@ var questions = [
     },
     {
         question: 'How many time zones are there in the world?',
-        answer: [
+        answers: [
             { text: '12', correct: false },
             { text: '36', correct: false },
             { text: '22', correct: false},
@@ -98,7 +135,7 @@ var questions = [
     },
     {
         question: 'Which is the largest land living mammal?',
-        answer: [
+        answers: [
             { text: 'Polar bear', correct: false },
             { text: 'Elephant', correct: true },
             { text: 'Hippo', correct: false},
@@ -107,7 +144,7 @@ var questions = [
     },
     {
         question: 'Which city has the largest population in the world?',
-        answer: [
+        answers: [
             { text: 'Delhi', correct: false },
             { text: 'Mexico City', correct: false },
             { text: 'Tokyo', correct: true},
